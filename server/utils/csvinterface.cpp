604b676a7fd5a350b4e7be5494ed1ecd249dc034
TheHolyException <theholyexception@gmail.com>
2023-03-14 09:49:11 +0100

    Adding getPlayerID and getPlayerScore for the CSVInterface

--- server/utils/csvinterface.cpp
+++ server/utils/csvinterface.cpp
@@ -10,6 +10,71 @@ QFile matchDB("./db/match.csv");
 QFile turnsDB("./db/turns.csv");
 
 /**
+ * @brief CSVInterface::getPlayerID Gets the PlayerID with the given name
+ *                                  If not exits, a new player will be created
+ * @param name of the Player
+ * @return PlayerID
+ */
+int CSVInterface::getPlayerID(QString name) {
+    int id = -1;
+    int lastID = -1;
+
+    if (playersDB.open(QIODevice::ReadOnly)) {
+       QTextStream is(&playersDB);
+
+       while (!is.atEnd()) {
+           QString line = is.readLine();
+           if (line.startsWith("#")) continue; // Skipping Header
+           QStringList lineData = line.split(',');
+
+           int lineid;
+           if ((lineid = lineData.at(0).toInt()) > lastID) lastID = lineid;
+           if (name.toLower() == lineData.at(1).toLower()) id = lineid;
+       }
+       playersDB.close();
+    }
+
+    if (id == -1 && playersDB.open(QIODevice::Append)) {
+        QTextStream os(&playersDB);
+        id = lastID +1;
+        os <<       QString::number(id) +
+              "," + name + ",0,0\n";
+        os.flush();
+        playersDB.close();
+    }
+
+    return id;
+}
+
+/**
+ * @brief CSVInterface::getPlayerScore Gets the 'TotalPlayCount' and 'TotalWinCount'
+ * @param playerid      Id of the Player
+ * @param totalPlayed   Pointer to the variable that stores the 'TotalPlayCount'
+ * @param totalWins     Pointer to the variable that stores the 'TotalWinCount'
+ * @returns true if the player score could be obtained, false when not
+ */
+bool CSVInterface::getPlayerScore(int playerid, int *totalPlayed, int *totalWins) {
+    if (playersDB.open(QIODevice::ReadOnly)) {
+        QTextStream is(&playersDB);
+
+        while (!is.atEnd()) {
+            QString line = is.readLine();
+            if (line.startsWith('#')) continue; // Skipping Header
+            QStringList lineData = line.split(',');
+
+            if (lineData.at(0).toInt() == playerid) {
+                *totalPlayed = lineData.at(2).toInt();
+                *totalWins = lineData.at(3).toInt();
+                return true;
+            }
+        }
+        playersDB.close();
+    }
+    return false;
+}
+
+
+/**
  * @brief CSVInterface::afterTurn should be executed after every turn to save all informations in the csv
  * @param matchid   ID of the Match
  * @param playerid  ID of the player
@@ -20,7 +85,6 @@ void CSVInterface::afterTurn(int matchid, int playerid, int fieldno) {
     QString turnDataRaw = "#turnno,matchid,playerid,fieldno\n";
     if (turnsDB.open(QIODevice::ReadOnly)) {
         QTextStream is(&turnsDB);
-        //is.readLine();
 
         while (!is.atEnd()) {
             QString line = is.readLine();
@@ -36,7 +100,7 @@ void CSVInterface::afterTurn(int matchid, int playerid, int fieldno) {
     }
 
     turnDataRaw +=
-            "" + QString::number(++lastTurn) +
+                   QString::number(++lastTurn) +
             ","  + QString::number(matchid) +
             ","  + QString::number(playerid) +
             ","  + QString::number(fieldno) + "\n";
@@ -56,31 +120,31 @@ void CSVInterface::afterMatch(int playerid1, int playerid2,
     // Writing matchdata to the match.csv
     if (matchDB.open(QIODevice::Append)) {
         QTextStream os(&matchDB);
-        os << ""   + QString::number(playerid1) +
-              ","  + QString::number(playerid2) +
-              ","  + QString::number(matchTime) +
-              ","  + QString::number(winnerid) + "\n";
+        os <<        QString::number(playerid1) +
+              "," + QString::number(playerid2) +
+              "," + QString::number(matchTime) +
+              "," + QString::number(winnerid) + "\n";
         os.flush();
         matchDB.close();
     }
 
-
-
     QString playerDataRaw = "#id,name,totalPlays,totalWins\n";
+
     // Reading all Playerdatas and increment the 'totalPlayed' count for every
     // Player played in this Match
     // also increment the 'totalWins' for the winner
     if (playersDB.open(QIODevice::ReadOnly)) {
         QTextStream is(&playersDB);
-        is.readLine(); // read first line to skip the header
         while(!is.atEnd()) {
-            //qDebug() << is.readLine();v
             QString line = is.readLine();
-            int id = line.split(",")[0].toInt();
-            QString name = line.split(",")[1];
-            int totalPlayed = line.split(",")[2].toInt();
-            int totalWins = line.split(",")[3].toInt();
+            if (line.startsWith('#')) continue; // Skipping Header
 
+            QStringList lineData = line.split(',');
+
+            int id          = lineData.at(0).toInt();
+            QString name    = lineData.at(1);
+            int totalPlayed = lineData.at(2).toInt();
+            int totalWins   = lineData.at(3).toInt();
 
             if (id == winnerid) totalWins ++;
             if (id == playerid1 || id == playerid2) totalPlayed++;
--- server/utils/csvinterface.h
+++ server/utils/csvinterface.h
@@ -10,6 +10,8 @@ class CSVInterface : public QObject {
 private:
     CSVInterface();
 public:
+    static int  getPlayerID(QString name);
+    static bool getPlayerScore(int playerid, int* totalPlayed, int* totalWins);
     static void afterTurn(int matchid, int playerid, int fieldno);
     static void afterMatch(int playerid1, int playerid2,
                            int winnerid, long matchTime);
